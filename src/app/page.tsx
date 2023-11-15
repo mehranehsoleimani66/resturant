1185e1221c3541094532fb8258751179f6cf59c4
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-15 11:19:40 +0330

    added slider/featured/offer to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Featured from "@/components/featured/Featured";
+import Offer from "@/components/offer/Offer";
+import Slider from "@/components/slider/Slider";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
+  );
 }
