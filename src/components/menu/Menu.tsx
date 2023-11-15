6343ef363520c3094d8080d796c63f1d9e5e2dd9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-15 11:21:11 +0330

    created menu component

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+import React, { useState } from "react";
+import CartIcon from "../cartIcon/CartIcon";
+
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+  const links = [
+    { id: 0, title: "HomePage", url: "/" },
+    { id: 1, title: "Menu", url: "/menu" },
+    { id: 2, title: "WorkingHours", url: "/" },
+    { id: 3, title: "HomePage", url: "/" }
+  ];
+  const user = false;
+  return (
+    <div>
+      {!open ? (
+        <Image
+          src="/open.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setOpen(true)}
+        />
+      ) : (
+        <Image
+          src="/close.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setOpen(false)}
+        />
+      )}
+
+      {open && (
+        <div className="absolute bg-red-500 w-full gap-8 h-[calc(100vh-6rem)] left-0 top-24 flex flex-col items-center justify-center text-white">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
+          ))}
+          {!user ? (
+            <Link href="/login" onClick={() => setOpen(false)}>
+              LogIn
+            </Link>
+          ) : (
+            <Link href="/order" onClick={() => setOpen(false)}>
+              Orders
+            </Link>
+          )}
+          <Link href="/cart" onClick={() => setOpen(false)}>
+            <CartIcon />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
