3b124de6f63abddef6476ada10fb0c153b032c7a
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-16 13:56:54 +0330

    modified footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,9 @@
-// import Link from "next/link";
-// import React from "react";
-
-// const Footer = () => {
-//   return (
-//     <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-//       <Link href="/" className="font-bold text-xl">
-//         MASSIMO
-//       </Link>
-//       <p>© ALL RIGHTS RESERVED.</p>
-//     </div>
-//   );
-// };
-
-// export default Footer;
 import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-green-500 flex items-center justify-between">
       <Link href="/" className="font-bold text-xl">
         MASSIMO
       </Link>
